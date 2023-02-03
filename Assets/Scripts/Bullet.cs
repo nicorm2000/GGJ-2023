a522420e1a1cf3a8ceea0c17881c78b3a14abad3
juliMAB <julian6aguirre@hotmail.com>
2023-02-03 13:11:58 -0300

    Pause Bullets

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -18,6 +18,9 @@ public class Bullet : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (PauseMenu.isPause)
+            return;
+
         if (target == null)
         {
             Destroy(gameObject);
