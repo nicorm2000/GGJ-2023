58df0a22cd7aa0ccf646ef8faea00bb397e08e6c
juliMAB <julian6aguirre@hotmail.com>
2023-02-05 16:20:29 -0300

    game over

--- Assets/Scripts/Menu Scrips/GameOverMenu.cs
+++ Assets/Scripts/Menu Scrips/GameOverMenu.cs
@@ -1,26 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameOverMenu : MonoBehaviour
 {
-    public GameObject gameOverMenuUI;
-
-    void Update()
-    {
-        //if(!playerAlive)
-        //{
-        //GameOver();
-        //}
-    }
-
-    public void GameOver()
-    {
-        gameOverMenuUI.SetActive(false);
-    }
     public void RestartGame()
     {
+        PauseMenu.isPause = false;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
     
